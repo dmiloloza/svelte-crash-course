ba11f0bebe3f4d309d8951ba0dc0bfe93db8e5c1
Domagoj Miloloza <dmiloloza@github.com>
2020-08-27 14:15:48 +0200

    show controls

--- README.md
+++
@@ -1,3 +0,0 @@
-# svelte-crash-course
-Traversy Media 
-Svelte Crash Course: https://youtu.be/uK2RnIzrQ0M
---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+    let name = 'John Doe';
+    let points = 100;
+    let showControls = false;
+
+    const addPoint = () => (points += 1);
+    const removePoint = () => (points -= 1);
+    const toggleControls = () => (showControls = !showControls);
+
+</script>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #204f6e;
+        text-transform: uppercase;
+        font-size: 2em;
+        font-weight: 100;
+    }
+
+    h3 {
+        margin-bottom: 10px;
+    }
+
+
+</style>
+
+<div class="container">
+    <div class="card">
+        <main>
+            <h1>
+                {name}
+                <button class="btn btn-sm" on:click={toggleControls}>+</button>
+            </h1>
+            <h3>{points}</h3>
+            {#if showControls}
+                <button class="btn" on:click={addPoint}>+1</button>
+                <button class="btn btn-dark" on:click={removePoint}>-1</button>
+                <input type="number" bind:value={points}>
+            {/if}
+        </main>
+    </div>
+</div>
+
+
